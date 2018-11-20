134661fe587817f556ed367f8a29d95304cf07e3
MyoMinHtwe <myomin.htwe@my.jcu.edu.au>
2018-11-20 14:09:03 +0800

    Input code

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """Hello, my name is Myo Min Htwe"""
+name = input("Enter name: ")
+while len(name) <1:
+    print ("Enter character: ")
+    name = input("Enter name")
+for i in range(0,len(name),2):
+    print(name[i])
